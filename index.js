9d6339a8f988be1ed2435ac231f55458e35d786c
llixtar <shelakov92@gmail.com>
2025-05-03 18:55:49 +0200

    imports up

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
+import { handleButtonClick } from './handleButtonClick.js';
+import { trackMousePosition } from './trackMousePosition.js';
+import { setupEventDelegation } from './setupEventDelegation.js';
+
 // ============   1   ============
 console.log('\n#1. Відстежування кліку на кнопку та виведення повідомлення.')
-import { handleButtonClick } from './handleButtonClick.js';
-
 handleButtonClick('myButton', 'Button clicked!');
-
 
 // ============   2   ============
 console.log('\n#2. Розробка функції відстеження позиції курсору миші.')
-import { trackMousePosition } from './trackMousePosition.js';
 trackMousePosition();
-
 
 // ============   3   ============
 console.log('\n#3. Реалізація делегування подій для відстеження кліків на елементах списку.')
-import { setupEventDelegation } from './setupEventDelegation.js';
 setupEventDelegation('#testList');
